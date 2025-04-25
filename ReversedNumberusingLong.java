f1546d2547219b1c82d3c234ca7e6feedf3c67cb
Rajvickram <vikramsachin400@gmail.com>
2025-04-25 17:03:13 +0530

    Recheck the Long Datatype using Reversed Number

--- ReversedNumberusingLong.java
+++ ReversedNumberusingLong.java
@@ -3,19 +3,20 @@ import java.util.Scanner;
 class ReversedNumberusingLong {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the Reveresed Number u Want to Reverese : ");
 
+        System.out.println("Enter the Reversed Number u need to Reverese : ");
+        
         long n = sc.nextLong();
 
-        System.out.println("Enter the Variable Name to store the Dummy Value to get Reverese Number series to Increment and to Get the Reverse Series : ");
+        System.out.println("Enter the Number to store the Dummy value to calculate the value to reversed the Upcoming User Value : ");
 
         long rev = sc.nextLong();
-
 
         while(n!=0) {
             rev = rev * 10 + n % 10;
             n = n / 10;
         }
-        System.out.println("Reversed Number : " + rev);
+
+        System.out.println("Reveresed Number : " + rev);
     }
 }
